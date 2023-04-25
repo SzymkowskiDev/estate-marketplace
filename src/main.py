1e74ea45ad59184cbf5cd1ab449add47060c3fe9
Kamil Szymkowski <szymkowski.k@gmail.com>
2023-04-25 20:42:35 +0200

    added networks

--- src/main.py
+++ src/main.py
@@ -12,25 +12,25 @@ conn = psycopg2.connect(
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
-# Create tables
-sql = """
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    email VARCHAR(50),
-    password VARCHAR(50)
-);
-"""
-cur.execute(sql)
+# # Create tables
+# sql = """
+# CREATE TABLE users (
+#     id SERIAL PRIMARY KEY,
+#     name VARCHAR(50),
+#     email VARCHAR(50),
+#     password VARCHAR(50)
+# );
+# """
+# cur.execute(sql)
 
-# Populate table with dummy data
-data = """
-INSERT INTO users (name, email, password) VALUES
-    ('Alice', 'alice@example.com', 'password1'),
-    ('Bob', 'bob@example.com', 'password2'),
-    ('Charlie', 'charlie@example.com', 'password3');
-"""
-cur.execute(data)
+# # Populate table with dummy data
+# data = """
+# INSERT INTO users (name, email, password) VALUES
+#     ('Alice', 'alice@example.com', 'password1'),
+#     ('Bob', 'bob@example.com', 'password2'),
+#     ('Charlie', 'charlie@example.com', 'password3');
+# """
+# cur.execute(data)
 
 # Query table
 cur.execute("SELECT * FROM users;")
