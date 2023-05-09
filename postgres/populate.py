a284b46f546a83e66ce1efb3190336551ce3b9e4
Kamil Szymkowski <szymkowski.k@gmail.com>
2023-05-09 22:21:06 +0200

    tidy up sql

--- postgres/populate.py
+++ postgres/populate.py
@@ -1,13 +1,12 @@
 import utilities
-import psycopg2
 
-# # Create tables
+# Create tables
 utilities.create_tables()
 
-# # Populate database
-utilities.populate_offers_db(file_path="postgres/offers_data.json")
-utilities.populate_buyers_db(file_path="postgres/buyers_data.json")
-utilities.populate_sellers_db(file_path="postgres/sellers_data.json")
+# Populate database
+utilities.populate_offers_db(file_path="postgres/dummy_data/offers_data.json")
+utilities.populate_buyers_db(file_path="postgres/dummy_data/buyers_data.json")
+utilities.populate_sellers_db(file_path="postgres/dummy_data/sellers_data.json")
 
 # Add randomly generated foreign key relations
 conn = utilities.connect_to_db()
@@ -15,12 +14,9 @@ cur = conn.cursor()
 
 sql = """
 ALTER TABLE OFFERS ADD seller_id INT;
-
 UPDATE OFFERS SET seller_id = FLOOR(RANDOM() * 3000) + 1;
-
 ALTER TABLE OFFERS
-ADD
-    CONSTRAINT FK_OFFERS_SELLERS FOREIGN KEY (seller_id) REFERENCES SELLERS(seller_id);
+ADD CONSTRAINT FK_OFFERS_SELLERS FOREIGN KEY (seller_id) REFERENCES SELLERS(seller_id);
 """
 
 cur.execute(sql)
