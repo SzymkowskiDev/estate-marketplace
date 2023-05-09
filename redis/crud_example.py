af50a8311b6f1bd6f9742ae3111aaa428e8c776a
Kamil Szymkowski <szymkowski.k@gmail.com>
2023-05-09 22:14:07 +0200

    developing redis data structure

--- redis/crud_example.py
+++ redis/crud_example.py
@@ -2,7 +2,24 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-r.set('mykey', 'myvalue')
+# Connect to Redis
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+# # Define a dictionary to store
+# mydict = {'date': '2002-10-10', 'seller_id': 30, 'buyer_id': 45, 'offer_id': 23}
+
+# # Set the dictionary in Redis as a hash
+# r.hmset('offer_23_seller_30_buyer_45_date_2002-10-10', mydict)
+
+# # Get the dictionary back from Redis
+# stored_dict = r.hgetall('myhash')
+
+# # Print the dictionary
+# print(stored_dict)
+
+r.set('offer_23_seller_30_buyer_45_date_2002-10-10', 'in progress')
+# values: placed (buyer asks to buy) -> accepted (seller accepted, awaiting payment) -> completed (buyer made payment)
+# also: 
 
 value = r.get('mykey')
 print(value)
